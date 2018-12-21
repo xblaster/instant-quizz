f5faac7ffbbca2cd521ac39318c5d51409256184
jerome <xblaster@gmail.com>
2018-12-21 13:32:22 +0000

    custome theme

--- instant-quizz/apps/front/src/app/app.module.ts
+++ instant-quizz/apps/front/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import {MatButtonModule} from '@angular/material/button';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, LandingComponent],
   imports: [
     BrowserModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
     NxModule.forRoot(),
     RouterModule.forRoot([
       {path : '', component: LandingComponent}
